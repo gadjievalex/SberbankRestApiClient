398e9b4d7c9e85cfb54efdd84b7b123f029c5d8b
AlyakseyHajieu <hajieualyaksey@gmail.com>
2019-05-13 17:08:24 +0300

    added
    added

---
+++ RestClient/Sberbank/Api/rest/Client.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestClient.Sberbank.Api.rest
+{
+    public class Client
+    {
+        public Client() { }
+    }
+}
---
+++ RestClient/Sberbank/Api/rest/RestApiSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestClient.Sberbank.Api.rest
+{
+    public class RestApiSettings
+    {
+    }
+}
